28385e76eff354590b1e281b2ac29bb354a94844
Carlo Gilmar <carlogilmar12@gmail.com>
2021-12-01 15:18:07 -0600

    Solvind 1st day, part 2

--- advent_of_code_2021/lib/2021/day1.ex
+++ advent_of_code_2021/lib/2021/day1.ex
@@ -1,16 +1,16 @@
 defmodule AofC2021.Day1 do
   @day_1_input "2021/day1.txt"
 
-  def solve_exercise(input) do
-    do_solution(input)
+  def solve_first_part(input) do
+    do_first_solution(input)
   end
 
-  def solve_exercise do
+  def solve_first_part do
     input = get_input()
-    do_solution(input)
+    do_first_solution(input)
   end
 
-  def do_solution(input) do
+  def do_first_solution(input) do
     input
     |> parse_to_integers()
     |> compare_with_previous_one()
@@ -34,6 +34,12 @@ defmodule AofC2021.Day1 do
     counters ++ [:increased]
   end
 
+  defp get_comparisions(first, second, tail, counters) when first == second do
+    counters = counters ++ [:nochange]
+    [first | [second | tail]] = tail
+    get_comparisions(first, second, [second | tail], counters)
+  end
+
   defp get_comparisions(first, second, tail, counters) when first > second do
     counters = counters ++ [:decreased]
     [first | [second | tail]] = tail
@@ -48,6 +54,39 @@ defmodule AofC2021.Day1 do
 
   defp count_increases(counters) do
     Enum.count(counters, fn i -> i == :increased end)
+  end
+
+  ############### Second Part
+
+  def solve_second_part(input) do
+    do_second_solution(input)
+  end
+
+  def solve_second_part() do
+    input = get_input()
+    do_second_solution(input)
+  end
+
+  def do_second_solution(input) do
+    input
+    |> parse_to_integers()
+    |> get_sums_list()
+    |> compare_with_previous_one()
+    |> count_increases()
+  end
+
+  def get_sums_list(input) do
+    build_sum_list(input, [])
+  end
+
+  def build_sum_list([_first, _second], sums_list) do
+    sums_list
+  end
+
+  def build_sum_list([first|[second|[third|tail]]], sums_list) do
+    sum = first + second + third
+    sums_list = sums_list ++ [sum]
+    build_sum_list([second|[third|tail]], sums_list)
   end
 
   def get_input() do
--- advent_of_code_2021/test/2021/day1_test.exs
+++ advent_of_code_2021/test/2021/day1_test.exs
@@ -2,13 +2,23 @@ defmodule Afc2021Test.Day1 do
   use ExUnit.Case
   alias AofC2021.Day1
 
-  test "Try the example input from description" do
+  test "First part: Try the example input from description" do
     input = ["199", "200", "208", "210", "200", "207", "240", "269", "260", "263"]
-    solution = Day1.solve_exercise(input)
+    solution = Day1.solve_first_part(input)
     assert solution == 7
   end
 
-  test "Getting the first part of the solution" do
-    assert 1448 == Day1.solve_exercise()
+  test "First part: Getting the first part of the solution" do
+    assert 1448 == Day1.solve_first_part()
+  end
+
+  test "Second part: Try example from description" do
+    input = ["199", "200", "208", "210", "200", "207", "240", "269", "260", "263"]
+    solution = Day1.solve_second_part(input)
+    assert solution == 5
+  end
+
+  test "Second part: Getting the first part of the solution" do
+    assert 1471 == Day1.solve_second_part()
   end
 end
