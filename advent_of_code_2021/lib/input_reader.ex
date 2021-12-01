25e0129b5cfcb85293c966b5368a127df9692fa3
Carlo Gilmar <carlogilmar12@gmail.com>
2021-12-01 12:48:53 -0600

    Adding input reader and day1 input

---
+++ advent_of_code_2021/lib/input_reader.ex
@@ -0,0 +1,11 @@
+defmodule InputReader do
+
+  def get_input(file_path) do
+    base_path = File.cwd!()
+    {:ok, text} = File.read("#{base_path}/priv/#{file_path}")
+    list = String.split(text, "\n")
+    [_empty | input] = Enum.reverse(list)
+		Enum.reverse(input)
+  end
+
+end
