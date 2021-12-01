752936fce30cbec37e7297ff2e918847fb5ec4a0
Carlo Gilmar <carlogilmar12@gmail.com>
2021-12-01 12:38:48 -0600

    Adding empty project for this year

---
+++ advent_of_code_2021/lib/afc2021.ex
@@ -0,0 +1,18 @@
+defmodule Afc2021 do
+  @moduledoc """
+  Documentation for `Afc2021`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Afc2021.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ advent_of_code_2021/test/afc2021_test.exs
@@ -0,0 +1,8 @@
+defmodule Afc2021Test do
+  use ExUnit.Case
+  doctest Afc2021
+
+  test "greets the world" do
+    assert Afc2021.hello() == :world
+  end
+end
