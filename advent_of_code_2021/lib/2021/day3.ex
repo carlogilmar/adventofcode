850c17f4ba7831522fa6debb2634aacb62124b92
Carlo Gilmar <carlogilmar12@gmail.com>
2021-12-03 23:56:13 -0600

    Adding day 3 second part

--- advent_of_code_2021/lib/2021/day3.ex
+++ advent_of_code_2021/lib/2021/day3.ex
@@ -63,6 +63,106 @@ defmodule AofC2021.Day3 do
   end
 
   defp do_second_solution(input) do
-    input
+    oxygen_rate = get_oxygen_rate(input)
+    co2_rate = get_co2_rate(input)
+    oxygen_rate * co2_rate
   end
+
+  defp get_oxygen_rate(input) do
+    [h|_tail] = input
+    size = h |> String.codepoints() |> length()
+
+    [found_number] =
+      Enum.reduce(0..(size - 1), input,
+        fn index, input ->
+          clean_input_by_bit_position(input, index)
+        end)
+
+    {oxygen_rate, ""} =
+      found_number
+      |> Integer.parse(2)
+
+    oxygen_rate
+  end
+
+  defp clean_input_by_bit_position(input, index) do
+    positions =
+      input
+      |> Enum.map(fn item ->
+        bit =
+          item
+          |> String.codepoints()
+          |> Enum.at(index)
+        {bit, item}
+      end)
+
+    bits_to_evaluate = Enum.map(positions, fn {pos, _item} -> pos end)
+
+    bit_champion =
+      case Enum.frequencies(bits_to_evaluate) do
+        %{"0" => zero, "1" => one} when zero > one -> "0"
+        %{"0" => zero, "1" => one} when zero < one -> "1"
+        %{"0" => zero, "1" => one} when zero == one -> "1"
+        _ -> :pivot
+      end
+
+    if bit_champion == :pivot do
+      positions
+      |> Enum.map(fn {_bit, item} -> item end)
+    else
+      positions
+      |> Enum.filter(fn {bit, _item} -> bit == bit_champion end)
+      |> Enum.map(fn {_bit, item} -> item end)
+    end
+  end
+
+  defp get_co2_rate(input) do
+    [h|_tail] = input
+    size = h |> String.codepoints() |> length()
+
+    [found_number] =
+      Enum.reduce(0..(size - 1), input,
+        fn index, input ->
+          clean_input_by_bit_position_for_co2(input, index)
+        end)
+
+    {rate, ""} =
+      found_number
+      |> Integer.parse(2)
+
+    rate
+  end
+
+  defp clean_input_by_bit_position_for_co2(input, index) do
+    positions =
+      input
+      |> Enum.map(fn item ->
+        bit =
+          item
+          |> String.codepoints()
+          |> Enum.at(index)
+        {bit, item}
+      end)
+
+    bits_to_evaluate = Enum.map(positions, fn {pos, _item} -> pos end)
+
+    bit_champion =
+      case Enum.frequencies(bits_to_evaluate) do
+        %{"0" => zero, "1" => one} when zero > one -> "1"
+        %{"0" => zero, "1" => one} when zero < one -> "0"
+        %{"0" => zero, "1" => one} when zero == one -> "0"
+        _ -> :pivot
+      end
+
+    if bit_champion == :pivot do
+      positions
+      |> Enum.map(fn {_bit, item} -> item end)
+    else
+      positions
+      |> Enum.filter(fn {bit, _item} -> bit == bit_champion end)
+      |> Enum.map(fn {_bit, item} -> item end)
+    end
+  end
+
+
 end
--- advent_of_code_2021/test/2021/day3_test.exs
+++ advent_of_code_2021/test/2021/day3_test.exs
@@ -22,7 +22,30 @@ defmodule Afc2021Test.Day3 do
   end
 
   test "First part: Try the input" do
-    assert 198 == Day3.solve_first_part()
+    assert 3549854 == Day3.solve_first_part()
+  end
+
+  test "Second Part: Try example input" do
+    input = [
+      "00100",
+      "11110",
+      "10110",
+      "10111",
+      "10101",
+      "01111",
+      "00111",
+      "11100",
+      "10000",
+      "11001",
+      "00010",
+      "01010"
+    ]
+    solution = Day3.solve_second_part(input)
+    assert solution == 230
+  end
+
+  test "Second Part: Try input" do
+    assert 230 == Day3.solve_second_part()
   end
 
 end
