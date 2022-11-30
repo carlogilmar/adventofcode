0d613683652a4e16da35ef235adf687cda9d3ce6
Carlo Gilmar <carlogilmar12@gmail.com>
2022-11-30 16:10:13 -0600

    Adding scripting

--- tools/lib/tools.ex
+++ tools/lib/tools.ex
@@ -1,42 +1,43 @@
 defmodule Tools do
-
   def run() do
     files = File.ls!(get_tests_path())
-		models_by_script = get_models_script()
-		for file <- files do
-			IO.inspect(file, label: "1) TEST TO CREATE:")
-			:ok = create_test_file(file, models_by_script)
-		end
+    models_by_script = get_models_script()
+
+    for file <- files do
+      IO.inspect(file, label: "1) TEST TO CREATE:")
+      :ok = create_test_file(file, models_by_script)
+    end
   end
 
-	def create_test_file(script_test_name, models_by_script) do
-		script_module = get_script_module(script_test_name)
-		domain_model = models_by_script[script_module]
+  def create_test_file(script_test_name, models_by_script) do
+    script_module = get_script_module(script_test_name)
+    domain_model = models_by_script[script_module]
 
-		IO.inspect(script_module, label: "2) SCRIPT MODULE:")
-		IO.inspect(domain_model, label: "3) DOMAIN MODULE:")
+    IO.inspect(script_module, label: "2) SCRIPT MODULE:")
+    IO.inspect(domain_model, label: "3) DOMAIN MODULE:")
 
-	  test_content = get_test_content(script_module, domain_model)
+    test_content = get_test_content(script_module, domain_model)
 
-		test_generated_path = "#{get_tests_generated_path()}/#{script_test_name}"
-	  :ok = File.write(test_generated_path, test_content)
-	end
+    test_generated_path = "#{get_tests_generated_path()}/#{script_test_name}"
+    :ok = File.write(test_generated_path, test_content)
+  end
 
-	def get_script_module(script_test_name) do
-		[module_name, ""] = String.split(script_test_name, "_script_test.exs")
-		if String.contains?(module_name, "_") do
-		  module_name
-			|> String.split("_")
-			|> Enum.map(fn word -> String.capitalize(word) end)
-			|> Enum.join("")
-		else
-			String.capitalize(module_name)
-		end
-	end
+  def get_script_module(script_test_name) do
+    [module_name, ""] = String.split(script_test_name, "_script_test.exs")
 
-	defp get_tests_generated_path do
+    if String.contains?(module_name, "_") do
+      module_name
+      |> String.split("_")
+      |> Enum.map(fn word -> String.capitalize(word) end)
+      |> Enum.join("")
+    else
+      String.capitalize(module_name)
+    end
+  end
+
+  defp get_tests_generated_path do
     "/Users/carlogilmar/Code/GitHub/adventofcode/tools/generated"
-	end
+  end
 
   defp get_tests_path do
     "/Users/carlogilmar/Code/ESL/pepsico/b2b-api-ex-v1/apps/api_v1/test/seeds/scripts"
@@ -72,38 +73,43 @@ defmodule Tools do
   end
 
   @doc """
-	%{
-	  "OrderAttempts" => "ApiV1.Orders.OrderAttempt",
-	  "Users" => "ApiV1.Users.User",
-	  "BrandPages" => "ApiV1.BrandPages.BrandPage",
-	  "SubmittedOrders" => "ApiV1.Orders.SubmittedOrder",
-	  "Rules" => "ApiV1.Rules.Rule"
-	}
+  %{
+   "OrderAttempts" => "ApiV1.Orders.OrderAttempt",
+   "Users" => "ApiV1.Users.User",
+   "BrandPages" => "ApiV1.BrandPages.BrandPage",
+   "SubmittedOrders" => "ApiV1.Orders.SubmittedOrder",
+   "Rules" => "ApiV1.Rules.Rule"
+  }
   """
   def get_models_script do
-		path = get_scripts_path()
-		scripts = File.ls!(path)
-		for script <- scripts, into: %{} do
-			content = File.read!("#{path}/#{script}")
-			get_model(content)
-		end
-	end
+    path = get_scripts_path()
+    scripts = File.ls!(path)
+
+    for script <- scripts, into: %{} do
+      content = File.read!("#{path}/#{script}")
+      get_model(content)
+    end
+  end
 
   def get_model(content) do
-		["" | [module_name|_tail]] = content |> String.split(["defmodule ApiV1.Scripts.", " do"])
-		model_name  = get_model_name(content)
-		{module_name, model_name}
-	end
+    ["" | [module_name | _tail]] = content |> String.split(["defmodule ApiV1.Scripts.", " do"])
+    model_name = get_model_name(content)
+    {module_name, model_name}
+  end
 
-	def get_model_name(content) do
-		[data_type_line] = content |> String.split("\n") |> Enum.filter(fn line -> String.contains?(line, "data_type:") end)
-		[model_name|_tail] = data_type_line |> String.split("data_type: ") |> Enum.reverse()
-		if String.contains?(model_name, ",") do
-			[model|_] = String.split(model_name, ",")
-			model
-		else
-			model_name
-		end
-	end
+  def get_model_name(content) do
+    [data_type_line] =
+      content
+      |> String.split("\n")
+      |> Enum.filter(fn line -> String.contains?(line, "data_type:") end)
 
+    [model_name | _tail] = data_type_line |> String.split("data_type: ") |> Enum.reverse()
+
+    if String.contains?(model_name, ",") do
+      [model | _] = String.split(model_name, ",")
+      model
+    else
+      model_name
+    end
+  end
 end
